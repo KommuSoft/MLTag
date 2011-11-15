34310001a63d6224f43a0d71f3b7101995d3e037
ingdas <ingdas@gmail.com>
2011-11-15 18:02:58 +0000

    git-svn-id: https://xp-dev.com/svn/MLTag@16 bccec01e-5d5c-4679-9fa7-4233b2d33955

--- code/MLTag/Recommender.cs
+++ code/MLTag/Recommender.cs
@@ -27,7 +27,7 @@ namespace MLTag
         private List<Recommender> rcs = new List<Recommender>();
         private Dictionary<Tag, int> dict = new Dictionary<Tag, int>();
         private IOrderedEnumerable<Tag> tags;
-        private readonly double treshold = 0.6d;
+        private readonly double threshold = 0.6d;
         private int nbTags;
 
         public int NumberOfTags {
@@ -85,7 +85,7 @@ namespace MLTag
             return ret
                 .Zip(tags, (x, y) => new Tuple<Tag, double>(y, x))
                 .OrderBy(x => x.Item2)
-                .Where(x => x.Item2 > treshold);
+                .Where(x => x.Item2 > threshold);
         }
     }
 }
