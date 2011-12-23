da6879a93eb5cd0ff8ae5ceb409e9f7d36a9962a
ingdas <ingdas@gmail.com>
2011-12-23 00:49:03 +0000

    git-svn-id: https://xp-dev.com/svn/MLTag@51 bccec01e-5d5c-4679-9fa7-4233b2d33955

---
+++ code/MLTag/Thesis.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace MLTag {
+    class Thesis {
+        private static string[] tags = new string[] { "dtai", "machinelearningendatamining", "distrinet", "veiligesoftware", "objectgerichtesoftwareontwikkeling", "dutch", "java", "practical", "voorstellingvanenredenerenmetkennis", "chihumancomputerinteractie", "hci", "english", "mobile", "ui", "nlp", "matlab", "gedistribueerdesoftware", "industrial", "programmeertalen", "logic", "declarativelanguages", "safety", "c", "cplusplus", "perl", "recommendersystem", "web", "computationeleinformatica", "experimental", "ehealth", "kr", "theoretical", "ocaml", "ruby", "bash", "python", "graphmining", "srl", "prolog", "urn", "uml", "softwarepatterns", "social", "w3c", "twr", "computervision", "problog", "nines", "sat", "textmining", "javascript", "php", "reo", "scala", "machinelearning", "clustering", "cp", "ilp", "abs", "imageprocessing", "gui", "video", "codes", "elearning", "computergraphicsenvisualisatie", "csharp", "chr", "logicprogramming", "nalag", "d", "eiffel", "lisp", "clojure", "groovy", "haskell", "music", "multidisciplinary", "visualization", "bioinformatics", "theoretischeinformatica", "computergraphics", "agda", "sport", "basketball", "erlang", "flash", "digitalization", "mda", "businesslogic", "xml", "occampi", "opendata", "fortran", "soccer" };
+
+        private static readonly Regex trainingRegex = new Regex(@"( +#([^# ]+))+ *$", RegexOptions.Compiled);
+        private static VotingSystem vs;
+        private static List<Metric> metrics = new List<Metric>();
+        private const int TRAIN_PERCENTAGE = 95;
+
+        public Thesis() {
+        }
+
+        public static void Train(string text, IList<string> tags) {
+            vs.Train(text, tags);
+        }
+        public static void Train(string line) {
+            string text;
+            List<String> tags = ParseTags(line, out text);
+            Train(text, tags);
+        }
+
+        public static IEnumerable<Tuple<string, double>> Recommend(string text) {
+            return vs.TagFiltered(text);
+        }
+        public static List<String> ParseTags(string line, out string text) {
+            Match m = trainingRegex.Match(line);
+            text = line.Substring(0, line.Length - m.Groups[0].Value.Length);
+            List<String> tags = new List<String>();
+            foreach (Capture c in m.Groups[2].Captures) {
+                tags.Add(c.Value);
+                //alltags.Add(c.Value);
+            }
+            return tags;
+        }
+        public static IEnumerable<Tuple<string, double>> Query(string line) {
+            Match m = trainingRegex.Match(line);
+            if (m.Success) {//training data
+                Train(line);
+                return null;
+            } else {//query
+                return Recommend(line);
+            }
+        }
+        public static void Test(string line) {
+            string text;
+            List<String> tags = ParseTags(line, out text);
+            IEnumerable<String> result = vs.TagFiltered(text).Select(x => x.Item1);
+            foreach (Metric m in metrics) {
+                m.Process(tags, result);
+            }
+        }
+
+        public static int Main(string[] args) {//run met "mono MLTag.exe trainfile+testfile ?logfile"
+            args = new string[] { "S:\\thesis.txt" };
+            metrics.Add(new TruePositivesMetric());
+            metrics.Add(new FalsePositivesMetric());
+            metrics.Add(new TrueNegativesMetric(tags.Length));
+            metrics.Add(new FalseNegativesMetric());
+            metrics.Add(new PrecisionMetric());
+            metrics.Add(new RecallMetric());
+            metrics.Add(new AccuracyMetric());
+            metrics.Add(new FMeasureMetric());
+            metrics.Add(new HammingLossMetric(tags.Length));
+            vs = new VotingSystem(tags);
+
+            //vs.AddRecommender(new NeuralNetworkRecommender());
+            //vs.AddRecommender (new CustomRecommender (tags.Length));
+            //vs.AddRecommender (new ID3Recommender(0.7d));
+            //vs.AddRecommender (new NearestNeighbourRecommender());
+
+            //vs.AddRecommender(new C45Recommender(tags));
+            //vs.AddRecommender(new MLkNNRecommender(tags.Count()));
+            vs.AddRecommender(new VectorClassif(tags.Count()));
+
+            Console.WriteLine("train");
+            Stream s = File.Open(args[0], FileMode.Open, FileAccess.Read);
+            List<string> test = new List<string>(readLines(s));
+            List<string> train = new List<string>();
+            s.Close();
+            int tp = TRAIN_PERCENTAGE * test.Count / 100;
+            Random rand = new Random();
+            for (int i = 0; i < tp; i++) {
+                int k = rand.Next(test.Count);
+                train.Add(test[k]);
+                test.RemoveAt(k);
+            }
+            #region Training
+            foreach (string l in train) {
+                Query(l);
+            }
+            vs.EndTrainingSession();
+            #endregion
+            #region TestInner
+            foreach (string l in train) {
+                Test(l);
+            }
+            Console.WriteLine("Inner Results:");
+            foreach (Metric m in metrics) {
+                Console.WriteLine("\t{0} = {1}", m.Name, m.Result);
+                m.Reset();
+            }
+            #endregion
+            #region TestOuter
+            foreach (string l in test) {
+                Test(l);
+            }
+            Console.WriteLine("Outer Results:");
+            foreach (Metric m in metrics) {
+                Console.WriteLine("\t{0} = {1}", m.Name, m.Result);
+            }
+            #endregion
+            #region testFree
+            string line = Console.ReadLine();
+            while (line != "exit") {
+                foreach (Tuple<string, double> kvp in vs.TagFiltered(line)) {
+                    Console.WriteLine("{0} / {1}", kvp.Item1, kvp.Item2);
+                }
+                Console.WriteLine("-----------------");
+                line = Console.ReadLine();
+            }
+            #endregion
+            return 0;
+        }
+
+        private static IEnumerable<string> readLines(Stream s) {
+            TextReader r = new StreamReader(s);
+            string line = r.ReadLine();
+            while (line != null) {
+                yield return line;
+                line = r.ReadLine();
+            }
+            r.Close();
+        }
+
+    }
+
+}
+
---
+++ code/MLTag/VectorClassif.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using weka.core;
+using java.util;
+using mulan.data;
+using weka.filters.unsupervised.attribute;
+using weka.classifiers.meta;
+using mulan.classifier.transformation;
+using mulan.classifier;
+using mulan.classifier.neural;
+using weka.filters;
+using weka.classifiers.bayes;
+using weka.classifiers.trees;
+
+namespace MLTag {
+    class VectorClassif : Recommender {
+        private Instances dataSet;
+        private Instances oDataSet;
+        private StringToWordVector stv;
+
+        public VectorClassif(int nbTags) {
+            tagsNb = nbTags;
+            ArrayList nomi = new ArrayList();
+            nomi.add("0");
+            nomi.add("1");
+            ArrayList attr = new ArrayList();
+            weka.core.Attribute stringAttr = new weka.core.Attribute("todoString", (List)null);
+            attr.add(stringAttr);
+            for (int i = 1; i <= nbTags; i++) {
+                attr.add(new weka.core.Attribute("label" + i, nomi));
+            }
+            oDataSet = new Instances("Todo-Instances", attr, 500);
+
+        }
+
+        private LabelsMetaData loadLabelsMeta(Instances data, int numLabels) {
+            LabelsMetaDataImpl labelsData = new LabelsMetaDataImpl();
+            int numAttributes = data.numAttributes();
+            for (int index = 1; index <= numLabels; index++) {
+                String attrName = "label" + index; // data.attribute(index).name();
+                labelsData.addRootNode(new LabelNodeImpl(attrName));
+            }
+            return labelsData;
+        }
+
+
+        private int tagsNb;
+        public int NumberOfTags {
+            get {
+                return tagsNb;
+            }
+            set {
+                tagsNb = value;
+            }
+        }
+
+        private BinaryRelevance lps;
+        public void EndTrainingSession() {
+            Console.WriteLine("End"); 
+            stv = new StringToWordVector();
+            stv.setAttributeNamePrefix("#");
+            stv.setLowerCaseTokens(true);
+            stv.setOutputWordCounts(true);
+            stv.setInputFormat(oDataSet);
+            stv.setStemmer(new weka.core.stemmers.LovinsStemmer());
+            stv.setIDFTransform(true);
+            dataSet = Filter.useFilter(oDataSet, stv);
+            MultiLabelInstances mli = new MultiLabelInstances(dataSet, loadLabelsMeta(dataSet, tagsNb));
+            
+            lps = new mulan.classifier.transformation.BinaryRelevance(new NaiveBayes());
+            lps.setDebug(true);
+            lps.build(mli);
+
+        }
+
+        public void Train(string text, IList<int> tags) {
+            
+            Instance ins = new DenseInstance(tagsNb + 1);
+            ins.setDataset(oDataSet);
+            ins.setValue(0, text);
+            for (int i = 0; i < tagsNb; i++) {
+                if (tags.Contains(i)) {
+                    ins.setValue(i + 1, 1);
+                } else {
+                    ins.setValue(i + 1, 0);
+                }
+
+            }
+            oDataSet.add(ins);
+        }
+
+        public IEnumerable<double> Tag(string text) {
+            Instance ins = new DenseInstance(tagsNb + 1);
+            ins.setDataset(oDataSet);
+            ins.setValue(0, text);
+            stv.input(ins);
+            ins = stv.output();
+            MultiLabelOutput mlo = lps.makePrediction(ins);
+            List<double> outp = new List<Double>();
+            foreach (bool b in mlo.getBipartition()) {
+                if (b) {
+                    outp.Add(1);
+                } else {
+                    outp.Add(0);
+                }
+            }
+            return outp;
+        }
+    }
+}
